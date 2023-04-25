bfe0d1bc0fcd019cb0d278d80ac8c842375ad249
Dmytro Kovryzhenko <onemoremagistr@gmail.com>
2023-04-25 23:41:12 +0300

    cleaning cpp classes

--- Source/ObstacleAssault/MovingPlatfrom.cpp
+++ Source/ObstacleAssault/MovingPlatfrom.cpp
@@ -19,9 +19,7 @@ void AMovingPlatfrom::BeginPlay()
 	SetActorLocation(CubeStartLocation);
 	SetActorScale3D(CubeScale);
 
-	// PlatformVelocity.X = (CubeEndLocation.X - (CubeStartLocation.X)) / TimeToMove;	  // 1/60 seconds per frame
-	// PlatformVelocity.Y = (CubeEndLocation.Y - (CubeStartLocation.Y)) / TimeToMove;
-	// PlatformVelocity.Z = ((CubeEndLocation.Z - 5223.0) + (CubeStartLocation.Z - 5223.0)) / TimeToMove;
+	// this->CalculateVelocity();
 }
 
 // Called every frame
@@ -29,53 +27,81 @@ void AMovingPlatfrom::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	// Move platform forwards
-	// Get current location
-	FVector CubeCurrentLocation = GetActorLocation();
-	// Add vector to that location
-	CubeCurrentLocation += PlatformVelocity * DeltaTime;
-	{
-		// if (CubeCurrentLocation.X < CubeEndLocation.X)
-		// 	CubeCurrentLocation.X += PlatformVelocity.X * DeltaTime;
-		// else if (CubeCurrentLocation.X > (CubeEndLocation.X + 1.0))
-		// 	CubeCurrentLocation.X -= 0.1 * PlatformVelocity.X * DeltaTime;
-		// if (CubeCurrentLocation.Y < CubeEndLocation.Y)
-		// 	CubeCurrentLocation.Y += PlatformVelocity.Y * DeltaTime;
-		// else if (CubeCurrentLocation.Y > (CubeEndLocation.Y + 1.0))
-		// 	CubeCurrentLocation.Y -= 0.1 * PlatformVelocity.Y * DeltaTime;
+	// this->GoToPoint();
 
-		// if ((CubeCurrentLocation.Z < 5223.0) && GoingUp)
-		// 	CubeCurrentLocation.Z -= PlatformVelocity.Z * DeltaTime;
-		// else if (CubeCurrentLocation.Z > CubeEndLocation.Z)
-		// {
-		// 	GoingUp = false;
-		// 	CubeCurrentLocation.Z += PlatformVelocity.Z * DeltaTime;
-		// }
-		// else if ((CubeCurrentLocation.Z < (CubeEndLocation.Z - 10.0)) && !GoingUp)
-		// {
-		// 	CubeCurrentLocation.Z -= 0.1 * PlatformVelocity.Z * DeltaTime;
-		// }
+	this->MovePlatform(DeltaTime);
+}
+
+void AMovingPlatfrom::MovePlatform(const float& DeltaTime)
+{
+	if (CheckOvershooting())	// Move platform forwards
+	{
+		// Get current location
+		FVector CubeCurrentLocation = GetActorLocation();
+		// Add vector to that location
+		CubeCurrentLocation += PlatformVelocity * DeltaTime;
+		// Set the new location
+		SetActorLocation(CubeCurrentLocation);
 	}
-	// Set the new location
-	SetActorLocation(CubeCurrentLocation);
-
-	// DistanceFromStart = static_cast<float>(FVector::Dist(CubeStartLocation, CubeCurrentLocation));
-
-	// Send platform back if gone to far
-
-	// Check how far we've moved
-	float DistanceMoved = FVector::Dist(CubeStartLocation, CubeCurrentLocation);
-
-	// Reverse direction of motion if gone too far
-	if (DistanceMoved > MoveDistance)
+	else	// Reverse direction of motion if gone too far
 	{
-		float OverShoot = DistanceMoved - MoveDistance;
-		// UE_LOG(LogTemp, Warning, TEXT("This body have overshoot: [%f]."), OverShoot);
-		V_LOGM(LogTemp, " has over shoot: %f", OverShoot);
+		// Check how far we've moved
+		// float OverShoot = DistanceMoved - MoveDistance;
+		// UE_LOG(LogTemp, Warning, TEXT("%s body have overshoot: [%f]."), *(this->GetName()), OverShoot);
+		// V_LOGM(LogTemp, " has over shoot: %f", OverShoot);
 
 		FVector MoveDirection = PlatformVelocity.GetSafeNormal();
 		CubeStartLocation += MoveDirection * MoveDistance;
 		SetActorLocation(CubeStartLocation);
 		PlatformVelocity *= -1;
 	}
+}
+
+bool AMovingPlatfrom::CheckOvershooting()
+{
+	float DistanceMoved = FVector::Dist(CubeStartLocation, GetActorLocation());
+	return DistanceMoved <= MoveDistance;
+}
+
+float AMovingPlatfrom::GetDistanceMoved() const { return FVector::Dist(CubeStartLocation, GetActorLocation()); }
+
+void AMovingPlatfrom::CalculateVelocity()
+{
+	// PlatformVelocity.X = (CubeEndLocation.X - (CubeStartLocation.X)) / TimeToMove;	  // 1/60 seconds per frame
+	// PlatformVelocity.Y = (CubeEndLocation.Y - (CubeStartLocation.Y)) / TimeToMove;
+	// PlatformVelocity.Z = ((CubeEndLocation.Z - 5223.0) + (CubeStartLocation.Z - 5223.0)) / TimeToMove;
+}
+
+void AMovingPlatfrom::GoToPoint()
+{
+	// // Get current location
+	// FVector CubeCurrentLocation = GetActorLocation();
+	//	// Add vector to that location
+	// CubeCurrentLocation += PlatformVelocity * DeltaTime;
+
+	// if (CubeCurrentLocation.X < CubeEndLocation.X)
+	// 	CubeCurrentLocation.X += PlatformVelocity.X * DeltaTime;
+	// else if (CubeCurrentLocation.X > (CubeEndLocation.X + 1.0))
+	// 	CubeCurrentLocation.X -= 0.1 * PlatformVelocity.X * DeltaTime;
+	// if (CubeCurrentLocation.Y < CubeEndLocation.Y)
+	// 	CubeCurrentLocation.Y += PlatformVelocity.Y * DeltaTime;
+	// else if (CubeCurrentLocation.Y > (CubeEndLocation.Y + 1.0))
+	// 	CubeCurrentLocation.Y -= 0.1 * PlatformVelocity.Y * DeltaTime;
+
+	// if ((CubeCurrentLocation.Z < 5223.0) && GoingUp)
+	// 	CubeCurrentLocation.Z -= PlatformVelocity.Z * DeltaTime;
+	// else if (CubeCurrentLocation.Z > CubeEndLocation.Z)
+	// {
+	// 	GoingUp = false;
+	// 	CubeCurrentLocation.Z += PlatformVelocity.Z * DeltaTime;
+	// }
+	// else if ((CubeCurrentLocation.Z < (CubeEndLocation.Z - 10.0)) && !GoingUp)
+	// {
+	// 	CubeCurrentLocation.Z -= 0.1 * PlatformVelocity.Z * DeltaTime;
+	// }
+
+	// Set the new location
+	// SetActorLocation(CubeCurrentLocation);
+
+	// DistanceFromStart = static_cast<float>(FVector::Dist(CubeStartLocation, CubeCurrentLocation));
 }
